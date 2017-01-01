aea63aa18e99b002937c8230e36d3e2143dfe41c
John <archfiery@gmail.com>
2017-01-01 21:13:03 +1100

    get all cases

--- Set1/FixedXOR.hs
+++ Set1/FixedXOR.hs
@@ -1,13 +1,18 @@
-module Set1.FixedXOR (xorString) where
+module FixedXOR (fixedXorString) where
 
 import           Data.Bits (xor)
 import           Data.Char
+import           Data.Hex  (hex)
+import           HexToBase64 (hexToStr)
 
-xorString :: String -> String -> String
-xorString [] []         = []
-xorString (x:xs) (y:ys) = xorChar x y : xorString xs ys
+fixedXorString :: String -> String -> String
+fixedXorString [] _          = []
+fixedXorString _ []          = []
+fixedXorString (x:xs) (y:ys) = xorChar x y : fixedXorString xs ys
 
 xorChar :: Char -> Char -> Char
-xorChar a b = intToDigit (digitToInt a `xor` digitToInt b)
+xorChar a b = chr (ord a `xor` ord b)
 
-main = print $ xorString "1c0111001f010100061a024b53535009181c" "686974207468652062756c6c277320657965" == "746865206b696420646f6e277420706c6179"
+main =
+  print . hex $ fixedXorString (hexToStr "1c0111001f010100061a024b53535009181c") (hexToStr "686974207468652062756c6c277320657965")
+  -- "746865206b696420646f6e277420706c6179"
--- Set1/SingleByteXORCipher.hs
+++ Set1/SingleByteXORCipher.hs
@@ -3,16 +3,18 @@ module SingleByteXORCipher (singleByteXor) where
 import           Control.Monad         (mapM_)
 import           Data.Bits             (xor)
 import qualified Data.ByteString.Char8 as C
-import           Data.Char             (digitToInt, intToDigit)
-import           Foreign.C.String      (castCCharToChar, castCharToCChar)
+import           Data.Char             (chr, digitToInt, intToDigit)
+import           Data.Hex              (hex, unhex)
+import           Data.Maybe            (fromMaybe)
+import           FixedXOR              (fixedXorString)
 import           HexToBase64           (hexToBase64, hexToStr)
 
+-- |print all the possible cases from key 32 to 127
 xorString :: String -> Int -> String
-xorString [] _ = []
-xorString (x:xs) c = intToDigit (digitToInt x `xor` c) : xorString xs c
+xorString a i = hex $ fixedXorString (fromMaybe "" (unhex a)) (replicate (length a) (chr i))
 
 singleByteXor :: String -> [String]
-singleByteXor s = map (xorString s) [0..15]
+singleByteXor s = map (xorString s) [32..127]
 
 main =
   mapM_ (putStrLn . hexToStr) . singleByteXor $ "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
